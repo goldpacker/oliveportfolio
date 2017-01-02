c89fe8d8a35f94a167600fcb21b4df04bd21a662
Clark Wang <w.xichao@gmail.com>
2017-01-01 17:59:19 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".button-collapse").sideNav();
+})
+
+$(document).ready(function(){
+  $('.carousel').carousel({
+    dist: -100, shift: 40
+  });
+});
+
+$(document).ready(function(){
+  $('.slider').slider({full_width: true});
+});
